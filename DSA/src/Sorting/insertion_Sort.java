b4e777f12c799b8fef4fe367a170041a45819aff
Shubham Dev <devshubham22@gmail.com>
2023-11-18 07:00:44 +0530

    18-11-23

---
+++ DSA/src/Sorting/insertion_Sort.java
@@ -0,0 +1,11 @@
+package Sorting;
+
+public class insertion_Sort {
+    public static void main(String[] args){
+        int[] arr= {5,4,3,1,2,-10,15,-15,22,13,21,25,26,10,100,-12};
+    }
+    static void insertion(int[] arr){
+
+        return arr;
+    }
+}
